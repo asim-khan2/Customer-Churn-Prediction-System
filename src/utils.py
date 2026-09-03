bb81ac9d8a923cf5aeeb798271d28aa8c74e239f
Asim Khan <asimkhan106988@gmail.com>
2026-09-03 22:51:05 +0530

    add data ingestion file

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import sys
+from sklearn.metrics import recall_score,f1_score,precision_score,accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+from src.logger import logging
+
+
+
+def evaluate_models(X_train,X_test,y_train,y_test,models,prams):
+
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = prams[list(models.keys())[i]]
+
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=param,
+                cv=3,
+                n_jobs=-1,
+                scoring='recall'
+            )
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = recall_score(y_train, y_train_pred)
+            test_model_score = recall_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+        
+    except Exception as e:
+        raise CustomException(e,sys)
